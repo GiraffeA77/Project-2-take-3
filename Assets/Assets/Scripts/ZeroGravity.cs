9783cdb27f9c8dc7159452f4f7f765c799a1fb0c
GamerOps117 <jsamueldalton@gmail.com>
2025-02-06 21:04:10 -0700

    testing script gravity

---
+++ Assets/Assets/Scripts/ZeroGravity.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZeroGravity : MonoBehaviour
+{
+    public List<GameObject> springs;
+    public Rigidbody rb;
+    public GameObject prop;
+    public GameObject CM;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.centerOfMass = CM.transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForceAtPosition(Time.deltaTime * transform.TransformDirection(Vector3.forward) * Input.GetAxis("Vertical") * 400f, prop.transform.position);
+        rb.AddTorque(Time.deltaTime * transform.TransformDirection(Vector3.up) * Input.GetAxis("Horizontal") * 300f);
+        foreach (GameObject spring in springs)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(spring.transform.position, transform.TransformDirection(Vector3.down), out hit, 3f))
+            {
+                rb.AddForceAtPosition(Time.deltaTime * transform.TransformDirection(Vector3.up) * Mathf.Pow(3f - hit.distance, 2) / 3f * 250f, spring.transform.position);
+            }
+            Debug.Log(hit.distance);
+        }
+        rb.AddForce(-Time.deltaTime * transform.TransformVector(Vector3.right) * transform.InverseTransformVector(rb.velocity).x * 5f);        
+    }
+}
