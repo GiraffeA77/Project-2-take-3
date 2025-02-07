6920cb77f8d4cc414220a9c844f2962f418e846d
GamerOps117 <jsamueldalton@gmail.com>
2025-02-06 22:01:30 -0700

    ZeroGravity Test 2.2

--- Assets/Assets/Scripts/ZeroGravity.cs
+++ Assets/Assets/Scripts/ZeroGravity.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,30 +6,29 @@ using UnityEngine;
 public class ZeroGravity : MonoBehaviour
 {
     public List<GameObject> springs;
-    public Rigidbody rb;
+    public Rigidbody b2;
     public GameObject prop;
     public GameObject CM;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb.centerOfMass = CM.transform.localPosition;
+        b2.centerOfMass = CM.transform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.AddForceAtPosition(Time.deltaTime * transform.TransformDirection(Vector3.forward) * Input.GetAxis("Vertical") * 400f, prop.transform.position);
-        rb.AddTorque(Time.deltaTime * transform.TransformDirection(Vector3.up) * Input.GetAxis("Horizontal") * 300f);
-        foreach (GameObject spring in springs)
-        {
+        b2.AddForceAtPosition(Time.deltaTime * transform.TransformDirection(Vector3.forward) * Input.GetAxis("Vertical") * 400f, prop.transform.position);
+        b2.AddTorque(Time.deltaTime * transform.TransformDirection(Vector3.up) * Input.GetAxis("Horizontal") * 300f);
+        foreach (GameObject spring in springs) { 
             RaycastHit hit;
             if (Physics.Raycast(spring.transform.position, transform.TransformDirection(Vector3.down), out hit, 3f))
             {
-                rb.AddForceAtPosition(Time.deltaTime * transform.TransformDirection(Vector3.up) * Mathf.Pow(3f - hit.distance, 2) / 3f * 250f, spring.transform.position);
+                b2.AddForceAtPosition(Time.deltaTime * transform.TransformDirection(Vector3.up) * Mathf.Pow(3f - hit.distance, 2) / 3f * 250f, spring.transform.position);
             }
             Debug.Log(hit.distance);
         }
-        rb.AddForce(-Time.deltaTime * transform.TransformVector(Vector3.right) * transform.InverseTransformVector(rb.velocity).x * 5f);        
+        b2.AddForce(-Time.deltaTime * transform.TransformVector(Vector3.right) * transform.InverseTransformVector(b2.velocity).x * 5f);
     }
 }
