ec7b370d076e26fdb9859b328fc1b0e427010160
GiraffeA77 <mrust1@arizona.edu>
2025-02-14 21:56:28 -0700

    move independantly.

--- Assets/Assets/Scripts/Movement.cs
+++ Assets/Assets/Scripts/Movement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public Camera activeCamera;
+
+    private Rigidbody rb;
     void Start()
     {
     }
@@ -17,28 +20,31 @@ public class Movement : MonoBehaviour
 
         // Set the game object's position
         transform.position = position;
+        if (activeCamera.gameObject.activeInHierarchy && activeCamera.enabled)
+        {
 
-        // Translate by 0.1m on Z axis each frame for as long as the W key is held down
-        if (Input.GetKey(KeyCode.W))
-            // Increment the game object's translation
-            transform.Translate(0, 0, 0.1f);
+            // Translate by 0.1m on Z axis each frame for as long as the W key is held down
+            if (Input.GetKey(KeyCode.W))
+                // Increment the game object's translation
+                transform.Translate(0, 0, 0.1f);
 
-        // Translate by 0.1m backwards on Z axis each frame for as long as the S key is held down
-        if (Input.GetKey(KeyCode.S))
-            // Increment the game object's translation
-            transform.Translate(0, 0, -0.1f);
+            // Translate by 0.1m backwards on Z axis each frame for as long as the S key is held down
+            if (Input.GetKey(KeyCode.S))
+                // Increment the game object's translation
+                transform.Translate(0, 0, -0.1f);
 
-        // Rotate by 1 degree on Y axis each frame for as long as the A key is held down
-        if (Input.GetKey(KeyCode.A))
-            // Rotate the game object to the left
-            transform.Rotate(Vector3.up, -1.0f);
+            // Rotate by 1 degree on Y axis each frame for as long as the A key is held down
+            if (Input.GetKey(KeyCode.A))
+                // Rotate the game object to the left
+                transform.Rotate(Vector3.up, -1.0f);
 
-        // Rotate by 1 degree on Y axis each frame for as long as the D key is held down
-        if (Input.GetKey(KeyCode.D))
-            // Rotate the game object to the right
-            transform.Rotate(Vector3.up, 1.0f);
+            // Rotate by 1 degree on Y axis each frame for as long as the D key is held down
+            if (Input.GetKey(KeyCode.D))
+                // Rotate the game object to the right
+                transform.Rotate(Vector3.up, 1.0f);
 
-        float moveVertical = Input.GetAxis("Vertical") * Time.deltaTime;
-        transform.Translate(0, 0, moveVertical);
+            float moveVertical = Input.GetAxis("Vertical") * Time.deltaTime;
+            transform.Translate(0, 0, moveVertical);
+        }
     }
 }
